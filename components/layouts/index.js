525051c3fcf5d944d9dc6676475c59f71deb3030
Mohamed Hamed <midking2013@gmail.com>
2022-10-04 18:13:54 +0200

    Remove console.log

--- components/layouts/index.js
+++ components/layouts/index.js
@@ -2,7 +2,6 @@ import { useRouter } from 'next/router';
 import Navbar from './Navbar';
 
 const Index = (props) => {
-  console.log(props);
   const router = useRouter();
   return (
     <div className={router.locale === 'ar' ? 'rtl' : null}>
